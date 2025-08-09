e57f310b4e11778e9661c5d479842b246e97c775
LG8700 <x@landongoodrich.com>
2025-08-09 14:07:40 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE = "viet-pom-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./words.json",
+  "./manifest.webmanifest",
+  "./IMG_4291.jpeg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
